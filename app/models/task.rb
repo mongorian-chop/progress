56b0b10025418c60d700c4eb00901f164a40bb49
Masaki KOMAGATA <komagata@gmail.com>
2010-07-07 15:51:47 +0900

    タスクのソート対応

--- app/models/task.rb
+++ app/models/task.rb
@@ -7,7 +7,8 @@ class Task < ActiveRecord::Base
   validates_length_of :name, :maximum => 255
   validates_length_of :description, :maximum => 255
 
-  named_scope :order_by, lambda {|column, order| {:order => "#{column} #{(order ? order : 'ASC')}"} }
+  named_scope :order_by, lambda {|column, order|
+    {:order => "#{(column ? column : 'id')} #{(order ? order : 'ASC')}"}}
 
   named_scope :with_properties,
     :select => 'tasks.*,
@@ -20,15 +21,15 @@ class Task < ActiveRecord::Base
       last_name',
     :joins  => [:status, :priority, :user]
 
-  def self.jsonize(project = nil)
-    (project ? project.tasks : Task).with_properties.map do |task|
-      task.localize.attributes
+  def self.jsonize(project = nil, sort = 'id', order = 'ASC')
+    (project ? project.tasks : Task).with_properties.order_by(sort, order).map do |task|
+      Task.localize(task.attributes)
     end
   end
 
-  def localize
-    self.priority_name = I18n.t("priority.#{self.priority_name}") if self.priority_name
-    self.status_name = I18n.t("status.#{self.status_name}") if self.status_name
-    self
+  def self.localize(hash)
+    hash['priority_name'] = I18n.t("priority.#{hash['priority_name']}") if hash['priority_name']
+    hash['status_name'] = I18n.t("status.#{hash['status_name']}") if hash['status_name']
+    hash
   end
 end
