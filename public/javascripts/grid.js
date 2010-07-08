f4a736ac12746b7d0f1e7ce211acd591dc3209ad
Tsukasa Hoki <tsukasa.hoki@gmail.com>
2010-07-08 12:09:46 +0900

    タスクデータのIDを修正

--- public/javascripts/grid.js
+++ public/javascripts/grid.js
@@ -216,7 +216,7 @@ var tasknav = {
             });
         },
         onclickSubmit: function(params, data){
-            this.url = "/tasks/"+data.id;
+            this.url = "/tasks/"+data.task_id;
             return {
                 'oper': "",
                 'task[id]': data.id,
