d67d6686ee82ab195784a8db2eb77bb7d009968e
Masaki KOMAGATA <komagata@gmail.com>
2010-07-19 14:54:14 +0900

    projectとuserのロードを遅延。

--- public/javascripts/chart.js
+++ public/javascripts/chart.js
@@ -2,232 +2,259 @@ $(function() {
   $('#logout').click(function(){ location.href = '/logout' }).button()
   $('#tabs').tabs()
 
-  // task
-  $('#gantt .action .add').button().click(function() {
-    $('<p>Add</p>').dialog({
-      title: L['Add'],
-      resizable: false,
-      draggable: false,
-      modal: true,
-      buttons: {
-        'Add': function() {
-          $(this).dialog('close')
-        },
-        'Cancel': function() {
-          $(this).dialog('close')
+  var load_gantt = function() {
+    $('#gantt .action .add').button().click(function() {
+      $('<p>Add</p>').dialog({
+        title: L['Add'],
+        resizable: false,
+        draggable: false,
+        modal: true,
+        buttons: {
+          'Add': function() {
+            $(this).dialog('close')
+          },
+          'Cancel': function() {
+            $(this).dialog('close')
+          }
         }
-      }
-    })
-  })
-  $('#gantt .action .edit').button().click(function() {
-    var project = $('.selected').data()
-    $('<form>')
-    .append($('<label>').text(L['activerecord']['attributes']['project']['name']))
-    .append($('<input>').val(project['name']))
-    .append($('<label>').text(L['activerecord']['attributes']['project']['start_on']))
-    .append($('<input>').val(project['start_on']))
-    .append($('<label>').text(L['activerecord']['attributes']['project']['end_on']))
-    .append($('<input>').val(project['end_on']))
-    .append($('<label>').text(L['activerecord']['attributes']['project']['description']))
-    .append($('<textarea>').val(project['description']))
-    .dialog({
-      title: L['Edit'],
-      resizable: false,
-      draggable: false,
-      modal: true,
-      buttons: {
-        'Edit': function() {
-          $(this).dialog('close')
-        },
-        'Cancel': function() {
-          $(this).dialog('close')
-        }
-      }
-    })
-  })
-  $('#gantt .action .delete').button().click(function() {
-    $('<p>'+L['Are you sure']+'</p>').dialog({
-      title: L['Delete'],
-      resizable: false,
-      draggable: false,
-      modal: true,
-      buttons: {
-        'Delete': function() {
-          $(this).dialog('close')
-        },
-        'Cancel': function() {
-          $(this).dialog('close')
-        }
-      }
-    })
-  })
-  $('#gantt .action .sort')
-  .append($('<option>').text('Name'))
-  .append($('<option>').text('Name (DESC)'))
-  .append($('<option>').text('Start On'))
-  .append($('<option>').text('Start On (DESC)'))
-  .append($('<option>').text('End On'))
-  .append($('<option>').text('End On (DESC)'))
-
-  $.getJSON('/projects', function(objects) {
-    var p = $("#gantt .action .project")
-    for (var i = 0; i < objects.length; i++) {
-      p.append($('<option>').text(objects[i]['name']).val(objects[i]['id']))
-    }
-  })
-
-  $.getJSON('/tasks', function(objects) {
-    var ganttData = []
-    for (var i = 0; i < objects.length; i++) {
-      var o = objects[i]
-      ganttData.push({
-        id:    o['id'],
-        name:  o['name'],
-        start: Date.parse(o['start_on'], 'yyyy-MM-dd'),
-        days:  o['days']
       })
-    }
+    })
+    $('#gantt .action .edit').button().click(function() {
+      var project = $('.selected').data()
+      $('<form>')
+      .append($('<label>').text(L['activerecord']['attributes']['project']['name']))
+      .append($('<input>').val(project['name']))
+      .append($('<label>').text(L['activerecord']['attributes']['project']['start_on']))
+      .append($('<input>').val(project['start_on']))
+      .append($('<label>').text(L['activerecord']['attributes']['project']['end_on']))
+      .append($('<input>').val(project['end_on']))
+      .append($('<label>').text(L['activerecord']['attributes']['project']['description']))
+      .append($('<textarea>').val(project['description']))
+      .dialog({
+        title: L['Edit'],
+        resizable: false,
+        draggable: false,
+        modal: true,
+        buttons: {
+          'Edit': function() {
+            $(this).dialog('close')
+          },
+          'Cancel': function() {
+            $(this).dialog('close')
+          }
+        }
+      })
+    })
+    $('#gantt .action .delete').button().click(function() {
+      $('<p>'+L['Are you sure']+'</p>').dialog({
+        title: L['Delete'],
+        resizable: false,
+        draggable: false,
+        modal: true,
+        buttons: {
+          'Delete': function() {
+            $(this).dialog('close')
+          },
+          'Cancel': function() {
+            $(this).dialog('close')
+          }
+        }
+      })
+    })
+  
+    $.getJSON('/projects', function(objects) {
+      var p = $("#gantt .action .project")
+      for (var i = 0; i < objects.length; i++) {
+        p.append($('<option>').text(objects[i]['name']).val(objects[i]['id']))
+      }
+    })
+  
+    $.getJSON('/tasks', function(objects) {
+      var ganttData = []
+      for (var i = 0; i < objects.length; i++) {
+        var o = objects[i]
+        ganttData.push({
+          id:    o['id'],
+          name:  o['name'],
+          start: Date.parse(o['start_on'], 'yyyy-MM-dd'),
+          end:   Date.parse(o['end_on'], 'yyyy-MM-dd'),
+          days:  o['days']
+        })
+      }
+      $('#gantt .content').empty()
+      $('#gantt .content').ganttView({
+        chartLang: {
+          days: L['date']['man_day'],
+          monthNames: L['date']['month_names'].slice(1, 12)
+        },
+        data: ganttData,
+        cellWidth: 21,
+        start: new Date(2010,5,1),
+        end: new Date(2010,11,1),
+        blockClick: function() {
+          console.log("click");
+        },
+        itemClick: function(data) {
+          console.log(data.id);
+        },
+        change: function(o,s,m) {
+        }
+      })
+    })
+  }
 
-    $('#gantt .content').empty()
-    $('#gantt .content').ganttView({
-      data: ganttData,
-      cellWidth: 21,
-      start: new Date(2010,5,1),
-      end: new Date(2010,12,31),
-      blockClick: function() {
-        console.log("click");
-      },
-      itemClick: function(data) {
-        console.log(data.id);
-      },
-      change: function(o,s,m) {
-      }
+  var load_project = function() {
+    $('#project .action .add').button().click(function() {
+      $('<p>Add</p>').dialog({
+        title: L['Add'],
+        resizable: false,
+        draggable: false,
+        modal: true,
+        buttons: {
+          'Add': function() {
+            $(this).dialog('close')
+          },
+          'Cancel': function() {
+            $(this).dialog('close')
+          }
+        }
+      })
     })
+    $('#project .action .edit').button().click(function() {
+      var project = $('.selected').data()
+      $('<form>')
+      .append($('<label>').text(L['activerecord']['attributes']['project']['name']))
+      .append($('<input>').val(project['name']))
+      .append($('<label>').text(L['activerecord']['attributes']['project']['start_on']))
+      .append($('<input>').val(project['start_on']))
+      .append($('<label>').text(L['activerecord']['attributes']['project']['end_on']))
+      .append($('<input>').val(project['end_on']))
+      .append($('<label>').text(L['activerecord']['attributes']['project']['description']))
+      .append($('<textarea>').val(project['description']))
+      .dialog({
+        title: L['Edit'],
+        resizable: false,
+        draggable: false,
+        modal: true,
+        buttons: {
+          'Edit': function() {
+            $(this).dialog('close')
+          },
+          'Cancel': function() {
+            $(this).dialog('close')
+          }
+        }
+      })
+    })
+    $('#project .action .delete').button().click(function() {
+      $('<p>'+L['Are you sure']+'</p>').dialog({
+        title: L['Delete'],
+        resizable: false,
+        draggable: false,
+        modal: true,
+        buttons: {
+          'Delete': function() {
+            $(this).dialog('close')
+          },
+          'Cancel': function() {
+            $(this).dialog('close')
+          }
+        }
+      })
+    })
+    $('#project .content').load_table_from_json('project', ['name', 'start_on', 'end_on', 'description'])
+  }
+
+  var load_user = function() {
+    $('#user .action .add').button().click(function() {
+      $('<p>Add</p>').dialog({
+        title: L['Add'],
+        resizable: false,
+        draggable: false,
+        modal: true,
+        buttons: {
+          'Add': function() {
+            $(this).dialog('close')
+          },
+          'Cancel': function() {
+            $(this).dialog('close')
+          }
+        }
+      })
+    })
+    $('#user .action .edit').button().click(function() {
+      var o = $('.selected').data()
+      $('<form>')
+      .append($('<label>').text(L['activerecord']['attributes']['user']['last_name']))
+      .append($('<input>').val(o['last_name']))
+      .append($('<label>').text(L['activerecord']['attributes']['user']['first_name']))
+      .append($('<input>').val(o['first_name']))
+      .append($('<label>').text(L['activerecord']['attributes']['user']['email']))
+      .append($('<input>').val(o['email']))
+      .append($('<label>').text(L['activerecord']['attributes']['user']['company']))
+      .append($('<input>').val(o['company']))
+      .append($('<label>').text(L['activerecord']['attributes']['user']['unit']))
+      .append($('<input>').val(o['unit']))
+      .append($('<label>').text(L['activerecord']['attributes']['user']['phone_number']))
+      .append($('<input>').val(o['phone_number']))
+      .dialog({
+        title: L['Edit'],
+        resizable: false,
+        draggable: false,
+        modal: true,
+        buttons: {
+          'Edit': function() {
+            $(this).dialog('close')
+          },
+          'Cancel': function() {
+            $(this).dialog('close')
+          }
+        }
+      })
+    })
+    $('#user .action .delete').button().click(function() {
+      $('<p>'+L['Are you sure']+'</p>').dialog({
+        title: L['Delete'],
+        resizable: false,
+        draggable: false,
+        modal: true,
+        buttons: {
+          'Delete': function() {
+            $(this).dialog('close')
+          },
+          'Cancel': function() {
+            $(this).dialog('close')
+          }
+        }
+      })
+    })
+    $('#user .content').load_table_from_json('user', ['login', 'last_name', 'first_name', 'email', 'company', 'unit', 'phone_number', 'last_login_at'])
+  }
+
+  switch (location.hash) {
+  case '#project':
+    load_project()
+    break;
+  case '#user':
+    load_user()
+    break;
+  case '#gantt':
+  default:
+    load_gantt()
+  } 
+
+  $('#tabs ul li a[href|=#gantt]').click(function () {
+    location.hash = ''
+    load_gantt()
   })
 
-  // project
-  $('#project .action .add').button().click(function() {
-    $('<p>Add</p>').dialog({
-      title: L['Add'],
-      resizable: false,
-      draggable: false,
-      modal: true,
-      buttons: {
-        'Add': function() {
-          $(this).dialog('close')
-        },
-        'Cancel': function() {
-          $(this).dialog('close')
-        }
-      }
-    })
+  $('#tabs ul li a[href|=#project]').click(function () {
+    location.href = '#project'
+    load_project()
   })
-  $('#project .action .edit').button().click(function() {
-    var project = $('.selected').data()
-    $('<form>')
-    .append($('<label>').text(L['activerecord']['attributes']['project']['name']))
-    .append($('<input>').val(project['name']))
-    .append($('<label>').text(L['activerecord']['attributes']['project']['start_on']))
-    .append($('<input>').val(project['start_on']))
-    .append($('<label>').text(L['activerecord']['attributes']['project']['end_on']))
-    .append($('<input>').val(project['end_on']))
-    .append($('<label>').text(L['activerecord']['attributes']['project']['description']))
-    .append($('<textarea>').val(project['description']))
-    .dialog({
-      title: L['Edit'],
-      resizable: false,
-      draggable: false,
-      modal: true,
-      buttons: {
-        'Edit': function() {
-          $(this).dialog('close')
-        },
-        'Cancel': function() {
-          $(this).dialog('close')
-        }
-      }
-    })
-  })
-  $('#project .action .delete').button().click(function() {
-    $('<p>'+L['Are you sure']+'</p>').dialog({
-      title: L['Delete'],
-      resizable: false,
-      draggable: false,
-      modal: true,
-      buttons: {
-        'Delete': function() {
-          $(this).dialog('close')
-        },
-        'Cancel': function() {
-          $(this).dialog('close')
-        }
-      }
-    })
-  })
-  $('#project .content').load_table_from_json('project', ['name', 'start_on', 'end_on', 'description'])
 
-  // user
-  $('#user .action .add').button().click(function() {
-    $('<p>Add</p>').dialog({
-      title: L['Add'],
-      resizable: false,
-      draggable: false,
-      modal: true,
-      buttons: {
-        'Add': function() {
-          $(this).dialog('close')
-        },
-        'Cancel': function() {
-          $(this).dialog('close')
-        }
-      }
-    })
+  $('#tabs ul li a[href|=#user]').click(function () {
+    location.href = '#user'
+    load_user()
   })
-  $('#user .action .edit').button().click(function() {
-    var o = $('.selected').data()
-    $('<form>')
-    .append($('<label>').text(L['activerecord']['attributes']['user']['last_name']))
-    .append($('<input>').val(o['last_name']))
-    .append($('<label>').text(L['activerecord']['attributes']['user']['first_name']))
-    .append($('<input>').val(o['first_name']))
-    .append($('<label>').text(L['activerecord']['attributes']['user']['email']))
-    .append($('<input>').val(o['email']))
-    .append($('<label>').text(L['activerecord']['attributes']['user']['company']))
-    .append($('<input>').val(o['company']))
-    .append($('<label>').text(L['activerecord']['attributes']['user']['unit']))
-    .append($('<input>').val(o['unit']))
-    .append($('<label>').text(L['activerecord']['attributes']['user']['phone_number']))
-    .append($('<input>').val(o['phone_number']))
-    .dialog({
-      title: L['Edit'],
-      resizable: false,
-      draggable: false,
-      modal: true,
-      buttons: {
-        'Edit': function() {
-          $(this).dialog('close')
-        },
-        'Cancel': function() {
-          $(this).dialog('close')
-        }
-      }
-    })
-  })
-  $('#user .action .delete').button().click(function() {
-    $('<p>'+L['Are you sure']+'</p>').dialog({
-      title: L['Delete'],
-      resizable: false,
-      draggable: false,
-      modal: true,
-      buttons: {
-        'Delete': function() {
-          $(this).dialog('close')
-        },
-        'Cancel': function() {
-          $(this).dialog('close')
-        }
-      }
-    })
-  })
-  $('#user .content').load_table_from_json('user', ['login', 'last_name', 'first_name', 'email', 'company', 'unit', 'phone_number', 'last_login_at'])
 })
--- public/javascripts/jquery.ganttView.js
+++ public/javascripts/jquery.ganttView.js
@@ -14,16 +14,13 @@ end: date
 cellWidth: number
 cellHeight: number
 slideWidth: number
-
-var ChartLang = {
-    days: "days",
-              monthNames: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
 };
 */
 var ChartLang = {
-    days: "人日",
-    monthNames: ["1月", "2月", "3月", "4月", "5月", "6月", "7月", "8月", "9月", "10月", "11月", "12月"]
+    days: "days",
+    monthNames: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
 };
+
 
 // points: array of [x, y] (float) representing relative break points
 function BrokenLineConnector(points){ this.points = points || [] }
@@ -38,7 +35,10 @@ BrokenLineConnector.prototype.paint = function(dims, ctx){
   ctx.lineTo(tx, ty);
   ctx.stroke();
 };
-
+function newDate(year ,month ,day){
+    if(month>0) month -= 1
+    return new Date(year,month,day)
+}
 (function (jQuery) {
   jQuery.fn.ganttView = function (options) {
     var els = this
@@ -46,7 +46,6 @@ BrokenLineConnector.prototype.paint = function(dims, ctx){
       showWeekends: true,
       cellWidth: 21,
       cellHeight: 21,
-      slideWidth: 400,
       vHeaderWidth: 100,
       blockClick: null,
       itemClick: null,
@@ -62,7 +61,10 @@ BrokenLineConnector.prototype.paint = function(dims, ctx){
                    new jsPlumb.Endpoints.Triangle({width: 8, height: 6})],
        endpointStyle: {fillStyle: "red"}
     }, options.connection);
-
+    if(opts.chartLang){
+        if(opts.chartLang.days && opts.chartLang.days.length > 0) ChartLang.days = opts.chartLang.days
+        if(opts.chartLang.monthNames && opts.chartLang.monthNames.length > 0) ChartLang.monthNames = opts.chartLang.monthNames
+    }
     els.each(function () {
       var container = jQuery(this)
       var div = jQuery("<div>", { "class": "ganttview" })
@@ -99,6 +101,7 @@ BrokenLineConnector.prototype.paint = function(dims, ctx){
 
     getMonths: function (start, end) {
       start = Date.parse(start); end = Date.parse(end)
+
       var months = []; months[start.getMonth()] = [start]
       var last = start
       while (last.compareTo(end) == -1) {
@@ -186,11 +189,11 @@ BrokenLineConnector.prototype.paint = function(dims, ctx){
       for (var i = 0; i < data.length; i++) {
         var series = data[i]
         if(!series.days) {
-          series.days = DateUtils.daysBetween(series.start, series.end)
+            series.days = DateUtils.daysBetween(series.start, series.end)
         }
         if (series.days && series.days > 0) {
           if (series.days > 365) { series.days = 365; } // Keep blocks from overflowing a year
-          var offset = DateUtils.daysBetween(start, series.start)
+          var offset = DateUtils.daysBetween(start, series.start ,true)
           var width = DateUtils.getWidth(series.start, series.days, cellWidth)
           var blockDiv = jQuery("<div>", {
             "id": "ganttview-block-" + series.id,
@@ -332,11 +335,21 @@ BrokenLineConnector.prototype.paint = function(dims, ctx){
   }
 
   var DateUtils = {
-    daysBetween: function (start, end) {
+    daysBetween: function (start, end , offsetMode) {
       if (!start || !end) { return 0; }
       start = Date.parse(start); end = Date.parse(end)
       if (start.getYear() == 1901 || end.getYear() == 8099) { return 0; }
       var count = 0, date = start.clone()
+      if(offsetMode){
+          var flg = true
+          while(flg) {
+            if(this.isWeekend(end)) {
+              end.addDays(1)
+            }else{
+              flg = false
+            }
+          }
+      }
       while (date.compareTo(end) == -1) { count = count + 1; date.addDays(1); }
       return count
     },
